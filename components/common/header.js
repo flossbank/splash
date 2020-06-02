bbc0641b7aca6062e6b747dfe2042302b6380361
Melissa Miller <meowwwls@users.noreply.github.com>
2020-06-01 23:53:00 -0500

    Fix nav logo render issues

--- components/common/header.js
+++ components/common/header.js
@@ -2,44 +2,20 @@ import { Box, Flex } from '@chakra-ui/core'
 
 import FBButton from '../common/button'
 import FBLogo from '../common/logo'
-import FBLogoLetters from '../common/logoLetters'
 import TextLink from './textLink'
-import useMedia from '../common/useMedia'
 
-export default () => {
-  const isWide = useMedia('(min-width: 800px')
-
-  // const HeaderButton = (props) => {
-  //   return (
-  //     <FBButton
-  //       backgroundColor="white"
-  //       margin="10px"
-  //       color="boulder"
-  //       width="auto"
-  //       variant="link"
-  //       onClick={props.onClick}
-  //     >
-  //       {props.children}
-  //     </FBButton>
-  //   );
-  // };
-
-  const logo = isWide ? (
-    <FBLogo />
-  ) : (
-    <FBLogoLetters width='2.25rem' height='auto' />
-  )
-
+const Header = () => {
   return (
     <>
       <Box as='header' padding={['1.5rem', '1.5rem', '2.5rem 5rem']}>
         <Flex alignItems='center' justify='space-between' flexDirection='row'>
-          {logo}
-          {/* TODO: convert to real nav and list / list items */}
-          <Flex
+          <FBLogo />
+          <Box
+            as='nav'
+            display='flex'
             flex='1'
             alignItems='center'
-            justify='space-between'
+            justifyContent='space-between'
             marginLeft={['2.25rem', 'auto']}
             maxW='22.5rem'
           >
@@ -56,9 +32,11 @@ export default () => {
             >
               Sign up
             </FBButton>
-          </Flex>
+          </Box>
         </Flex>
       </Box>
     </>
   )
 }
+
+export default Header
--- components/common/logoLetters.js
+++ components/common/logoLetters.js
@@ -1,9 +1,10 @@
 import { Box } from '@chakra-ui/core'
 
+// TODO: get inline SVG and replace
 const FBLogoLetters = ({ margin = 0, width = '2.5rem', height = 'auto' }) => {
   return (
     <Box margin={margin} width={width} height={height}>
-      <img src='/images/fbLogoLetters.png' alt='Flossbank' />
+      <img src='/images/fbLogoLetters.png' alt='Flossbank logo' />
     </Box>
   )
 }
