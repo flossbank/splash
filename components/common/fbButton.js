a0e783a8bdc104322bbddc225fa97ec9fa1a933d
Melissa Miller <meowwwls@users.noreply.github.com>
2020-06-07 19:55:57 -0500

    Separate LinkBtn and FBButton to utilize isLoading state

--- components/common/fbButton.js
+++ components/common/fbButton.js
@@ -1,15 +1,15 @@
-import { Button } from "@chakra-ui/core";
-import { defaultProps } from "../../utils/defaultBtnProps";
-import PropTypes from "prop-types";
+import { Button } from '@chakra-ui/core'
+import { defaultProps } from '../../utils/defaultBtnProps'
+import PropTypes from 'prop-types'
 
 const FBButton = ({ children, ...props }) => (
   <Button {...props}>{children}</Button>
-);
+)
 
-FBButton.defaultProps = { ...defaultProps };
+FBButton.defaultProps = { ...defaultProps }
 
 FBButton.PropTypes = {
-  onClick: PropTypes.func,
-};
+  onClick: PropTypes.func
+}
 
-export default FBButton;
+export default FBButton
