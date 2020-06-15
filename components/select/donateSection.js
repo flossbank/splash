dd89abb7e522774e25f53577b2cf064bf4ac348b
Melissa Miller <meowwwls@users.noreply.github.com>
2020-06-15 17:54:42 -0500

    Refine donation section

---
+++ components/select/donateSection.js
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/router'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements, ElementsConsumer } from '@stripe/react-stripe-js'
+
+import { Text } from '@chakra-ui/core'
+
+import DonateForm from './donateForm'
+import Section from '../common/section'
+import UnderlinedHeading from '../common/underlinedHeading'
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
+
+const InjectedDonateForm = ({ handleSuccess }) => (
+  <ElementsConsumer>
+    {({ elements, stripe }) => (
+      <DonateForm
+        elements={elements}
+        stripe={stripe}
+        handleSuccess={handleSuccess}
+      />
+    )}
+  </ElementsConsumer>
+)
+
+const DonateSection = ({ seeAds }) => {
+  const router = useRouter()
+
+  const handleDonationSuccess = () => {
+    router.push('/install')
+  }
+
+  return (
+    <Section
+      textAlign='center'
+      backgroundColor='lightRock'
+      id='donate-form'
+      paddingBottom={{ lg: '6rem' }}
+    >
+      <UnderlinedHeading text='Payment Information' align='center' />
+      <Text fontSize='1.5rem'>
+        Set a monthly contribution and input your card information.
+      </Text>
+      <Elements stripe={stripePromise}>
+        <InjectedDonateForm
+          handleSuccess={handleDonationSuccess}
+          seeAds={seeAds}
+        />
+      </Elements>
+    </Section>
+  )
+}
+
+export default DonateSection
--- components/select/select_tier_section.js
+++ components/select/select_tier_section.js
@@ -1,25 +1,12 @@
 import { Box, Flex, Text } from '@chakra-ui/core'
-import { useRouter } from 'next/router'
 import { useState } from 'react'
-import { Elements, ElementsConsumer } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
 
 import Section from '../common/section'
 import UnderlinedHeading from '../common/underlinedHeading'
 import Subheading from '../common/subheading'
 import StepperSection from '../common/stepperSection'
 import SelectTierCards from './select_tier_cards'
-import DonateForm from './donate_form'
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
-
-const InjectedDonateForm = ({ handleSuccess }) => (
-  <ElementsConsumer>
-    {({ elements, stripe }) => (
-      <DonateForm elements={elements} stripe={stripe} handleSuccess={handleSuccess} />
-    )}
-  </ElementsConsumer>
-)
+import DonateSection from './donateSection'
 
 const steps = [
   {
@@ -37,7 +24,6 @@ const steps = [
 ]
 
 const SelectTierSection = () => {
-  const router = useRouter()
   const [showDonateForm, setShowDonateForm] = useState(false)
   const [seeAds, setSeeAds] = useState(false)
 
@@ -45,10 +31,6 @@ const SelectTierSection = () => {
     if (!withDonation) return
     if (seeAds) setSeeAds(true)
     setShowDonateForm(true)
-  }
-
-  const handleDonationSuccess = () => {
-    router.push('/install')
   }
 
   return (
@@ -89,20 +71,7 @@ const SelectTierSection = () => {
         <Flex flexDirection={['column', 'row']} margin='2rem 2rem 5rem 2rem'>
           <SelectTierCards onSelected={handleOnSelected} />
         </Flex>
-        {showDonateForm && (
-          <Box
-            as='section'
-            id='donate-form'
-            padding={['2rem 1rem 5rem 1rem', '2rem 20% 5rem 20%']}
-            textAlign='center'
-            backgroundColor='puddle'
-          >
-            <UnderlinedHeading text='Payment info' align='center' />
-            <Elements stripe={stripePromise}>
-              <InjectedDonateForm handleSuccess={handleDonationSuccess} seeAds={seeAds} />
-            </Elements>
-          </Box>
-        )}
+        {showDonateForm && <DonateSection seeAds={seeAds} />}
       </Box>
     </Section>
   )
