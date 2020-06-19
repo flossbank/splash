d568dc9f1dda95a581be88d5b0bea8c7cc100f19
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-18 22:08:25 -0700

    added in fetching installed packages for user dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-
-import PageWrapper from '../components/common/pageWrapper'
+import { useState, useEffect } from 'react'
 import { Text, Box, Heading, Flex, List, ListItem, CircularProgress, Alert, AlertIcon } from '@chakra-ui/core'
 
+import PageWrapper from '../components/common/pageWrapper'
+import { fetchUserInstalledPackages } from '../client'
+
 const Dashboard = () => {
-  /* eslint-disable */
+  const [packagesTouchedLoading, setPackagesTouchedLoading] = useState(true)
   const [packagesTouched, setPackagesTouched] = useState()
-  const [donation, setDonation] = useState()
-  const [moneyGenerated, setMoneyGenerated] = useState()
-  /* eslint-enable */
+  const [donation] = useState()
+
+  async function fetchData() {
+    try {
+    const data = await fetchUserInstalledPackages()
+    if (data) {
+      const packagesTouchedData = data.installedPackages.reduce((acc, pkg) => acc + pkg.installCount, 0)
+      setPackagesTouched(packagesTouchedData)
+      setPackagesTouchedLoading(false)
+    }
+    } catch (e) {
+      setPackagesTouched('Error')
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [0]) // only run on mount
 
   return (
     <PageWrapper
@@ -58,19 +74,9 @@ const Dashboard = () => {
                 backgroundColor='white'
                 padding='2.5rem 2.25rem'
               >
-                {!packagesTouched && <CircularProgress isIndeterminate color='ocean' />}
+                {!packagesTouched && <CircularProgress isIndeterminate={packagesTouchedLoading} color='ocean' />}
+                <Heading>{packagesTouched}</Heading>
                 <Text>Packages touched</Text>
-              </ListItem>
-              <ListItem
-                className='u-box-shadow'
-                display='flex'
-                flexDirection={['column']}
-                alignItems='center'
-                backgroundColor='white'
-                padding='2.5rem 2.25rem'
-              >
-                {!moneyGenerated && <CircularProgress isIndeterminate color='ocean' />}
-                <Text>Generated for open source</Text>
               </ListItem>
               <ListItem
                 className='u-box-shadow'
