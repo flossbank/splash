140abf4d156169f01d619ad98a2cb7ee3c35cf9a
Melissa Miller <meowwwls@users.noreply.github.com>
2020-06-22 11:38:30 -0500

    Clean up styles on Verify page

--- components/verify/verify_humanity.js
+++ components/verify/verify_humanity.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect, useMemo } from 'react'
-import { Box, Heading, Flex, Text } from '@chakra-ui/core'
-import { decode } from 'b36'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
+import { decode } from 'b36'
 import Recaptcha from 'react-recaptcha'
 import { verifyRegistration } from '../../client'
-import FBDivider from '../common/divider'
+
+import { Flex, Text } from '@chakra-ui/core'
+
 import StepperSection from '../common/stepperSection'
+import Section from '../common/section'
+import UnderlinedHeading from '../common/underlinedHeading'
+import Subheading from '../common/subheading'
+import TextLink from '../common/textLink'
+import ErrorMessage from '../common/errorMessage'
 
 const VerifyHumanity = () => {
   const router = useRouter()
@@ -14,17 +20,20 @@ const VerifyHumanity = () => {
   const [email, setEmail] = useState('')
   const [token, setToken] = useState('')
   const [status, setStatus] = useState('')
+  const [hasError, setHasError] = useState(false)
   const showCaptcha = useMemo(() => email && token, [email, token])
 
   useEffect(() => {
-    const { e: encodedEmail, token } = (router.query || {})
+    const { e: encodedEmail, token } = router.query || {}
     const noQueryParams = !window.location.search
     const invalidQueryParams = !encodedEmail || !token
     if ((invalidQueryParams && queryMiss) || noQueryParams) {
-      setStatus('Failure: no email or token in URL, contact support@flossbank.com for assistance.')
+      setHasError(true)
+      setStatus('Failure: no email or token in URL. ')
     } else if (invalidQueryParams) {
       setQueryMiss(true)
     } else {
+      setHasError(false)
       setStatus('Verifying email...')
       setEmail(decode(encodedEmail).toString())
       setToken(token)
@@ -34,12 +43,12 @@ const VerifyHumanity = () => {
   const verify = async (response) => {
     try {
       await verifyRegistration({ email, token, response })
+      setHasError(false)
       setStatus('Success!')
       router.push('/select')
     } catch (e) {
-      setStatus(
-        'Failed to verify email. Please try again or contact support@flossbank.com for assistance.'
-      )
+      setHasError(true)
+      setStatus('Failed to verify email. ')
     }
   }
 
@@ -59,41 +68,60 @@ const VerifyHumanity = () => {
   ]
 
   return (
-    <Box height='85vh'>
+    <>
       <Head>
         <script src='https://www.google.com/recaptcha/api.js' async defer />
       </Head>
       <StepperSection steps={steps} currentStep={1} />
-      <Box padding={['0 1rem 0 1rem', '0 0 0 0']}>
-        <Heading
-          textTransform='uppercase'
-          fontWeight='bold'
-          fontSize='14px'
-          textAlign='center'
-          marginBottom='1rem'
-        >
-          Verify
-        </Heading>
-        <FBDivider margin='auto' />
-        <Text fontSize='24px' textAlign='center' margin='2rem'>
-          No robots allowed
+      <h1 className='sr-only'>Verify your Flossbank account</h1>
+      <Section
+        textAlign='center'
+        padding={{ base: '1.5rem 1.5rem 3rem', lg: '1rem 3rem 4rem' }}
+        aria-live='polite'
+      >
+        <UnderlinedHeading text='Verify' align='center' aria-hidden='true' />
+        <Subheading>No robots allowed</Subheading>
+        <Text marginBottom='1rem'>
+          On behalf of the open source community, thank you for installing!
+          &hearts;
         </Text>
-        <Text textAlign='center' marginBottom='1rem'>
-          On behalf of the open source community, thank you for installing! ♥
-        </Text>
-        <Text textAlign='center' fontWeight='bold'>{status}</Text>
+        {!hasError && (
+          <Text fontWeight='bold' marginBottom='1.5rem' color='ocean'>
+            {status}
+          </Text>
+        )}
         {showCaptcha && (
-          <Flex flexDirection='row' justify='center' marginTop='3rem'>
+          <Flex direction='row' justify='center' marginTop='3rem'>
             <Recaptcha
               render='explicit'
-              onloadCallback={() => setStatus('Please complete the captcha below to verify you\'re a human.')}
+              onloadCallback={() =>
+                setStatus(
+                  'Please complete the captcha below to verify you\'re a human.'
+                )}
               verifyCallback={verify}
               sitekey={process.env.RECAPTCHA_SITE_KEY}
             />
           </Flex>
         )}
-      </Box>
-    </Box>
+        {hasError && (
+          <ErrorMessage
+            msg={status}
+            maxW='30rem'
+            iconSz='2rem'
+            margin='3rem auto 0'
+            color='boulder'
+            padding='1.5rem !important'
+          >
+            Please try again or contact{' '}
+            <TextLink
+              text='support@flossbank.com'
+              href='mailto:support@flossbank.com'
+            />{' '}
+            for assistance.
+          </ErrorMessage>
+        )}
+      </Section>
+    </>
   )
 }
 
