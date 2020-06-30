7b58adaf71c9d39b31625bb85da9fed2d7435349
Joel Wasserman <joel.wasserman8@gmail.com>
2020-06-29 20:10:58 -0700

    added use local storage hook, sort packages alphabetically, and add in two cards

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,5 @@
+export const localStorageDashboardWelcomeBannerKey = 'flossbank_u_show_welcome_banner'
+
 export const whitelistedEndpoints = [
   '/signup',
   '/login',
---
+++ utils/downloader.js
@@ -0,0 +1,7 @@
+export const downloadData = (data, filename) => {
+  const a = document.createElement('a')
+  a.setAttribute('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(data))
+  a.setAttribute('download', filename)
+  a.click()
+}
+
