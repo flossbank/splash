a5e5145f729d58ce8d161f9269594b71ddcd7df5
Joel Wasserman <joel.wasserman8@gmail.com>
2020-07-10 13:15:09 -0700

    address sec concern with target blank

--- components/common/textLink.js
+++ components/common/textLink.js
@@ -14,7 +14,7 @@ const TextLink = ({ href, text, children, external, ...props }) => (
         </ChakraLink>
       </Link>
     ) : (
-      <ChakraLink href={href} target='_blank' className={styles.link} {...props}>
+      <ChakraLink href={href} target='_blank' rel='noopener noreferrer' className={styles.link} {...props}>
         {text}
       </ChakraLink>
     )}
