f3e96c0c43482cc982faae05ff1a1bbd9233d23b
Joel Wasserman <joel.wasserman8@gmail.com>
2020-07-13 21:38:46 -0700

    added in some user info to settings page

--- components/user/settings/settingsSection.js
+++ components/user/settings/settingsSection.js
@@ -4,30 +4,48 @@ import Section from '../../common/section'
 import Card from '../../common/card'
 import UnderlinedHeading from '../../common/underlinedHeading'
 
-const UserSettingsSection = ({ ...props }) => (
-  <Section
-    display='flex'
-    justifyItems='center'
-    flexDirection='column'
-    alignItems='center'
-    padding={{ base: '3rem 1.5rem', lg: '6rem 7.5rem' }}
-    backgroundColor='white'
-    {...props}
-  >
-    <UnderlinedHeading text='User Settings' align='center' marginBottom='3rem' />
-    <Card shadowSz='lg' w='100%' maxW='45rem'>
-      <Text
-        textTransform='uppercase'
-        textAlign={{ base: 'center', lg: 'left' }}
-        color='ocean'
-        fontWeight='bold'
-        fontSize='0.875rem'
-        marginBottom='2.5rem'
-      >
-        Settings
-      </Text>
-    </Card>
-  </Section>
-)
+import { useAuth } from '../../../utils/useAuth'
+
+const UserSettingsSection = ({ ...props }) => {
+  const { user } = useAuth()
+
+  return (
+    <Section
+      display='flex'
+      justifyItems='center'
+      flexDirection='column'
+      alignItems='center'
+      padding={{ base: '3rem 1.5rem', lg: '4rem 7.5rem' }}
+      backgroundColor='white'
+      {...props}
+    >
+      <UnderlinedHeading text='User Settings' align='center' marginBottom='3rem' />
+      <Card shadowSz='lg' w='100%' maxW='80%'>
+        <Text
+          textTransform='uppercase'
+          textAlign={{ base: 'center', lg: 'left' }}
+          color='ocean'
+          fontWeight='bold'
+          fontSize='0.875rem'
+          marginBottom='1.5rem'
+        >
+          Billing info
+        </Text>
+        <Text>
+          Email
+        </Text>
+        <Text>
+          {user.email}
+        </Text>
+        <Text>
+          Payment method
+        </Text>
+        <Text>
+          {user.billingInfo.last4 ? `****-****-****-${user.billingInfo.last4}` : 'N/a'}
+        </Text>
+      </Card>
+    </Section>
+  )
+}
 
 export default UserSettingsSection
