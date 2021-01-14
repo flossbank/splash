4782c0b4e889a23acf2f42b0850334d48cd2ba2c
Joel Wasserman <joel.wasserman8@gmail.com>
2021-01-14 11:11:03 -0800

    remove fetch localhost

--- client/fetch.js
+++ client/fetch.js
@@ -1,7 +1,7 @@
 import got from 'got'
 
 export default got.extend({
-  prefixUrl: 'http://localhost:8081',
+  prefixUrl: process.env.NEXT_PUBLIC_PREVIEW ? process.env.STAGING_API_HOST : process.env.API_HOST,
   throwHttpErrors: true,
   responseType: 'json',
   headers: {
